c629dc50c5597a1148c970ffc7d59eb1f656609c
xoxokss <sangseon14@gmail.com>
2022-06-19 00:41:44 +0900

    인증 미들웨어 수정

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,17 +1,18 @@
 const jwt = require("jsonwebtoken");
-const userDB = require("../models/user");
+const User = require("../models/user");
+
 module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
   const [tokenType, tokenValue] = (authorization || "").split(" ");
-  if (!tokenValue || tokenType !== "Bearer") {
+  if (tokenType !== "Bearer") {
     res.status(401).send({
-      errorMessage: "로그인이 필요한 페이지 입니다.",
+      errorMessage: "로그인이 필요합니다.",
     });
     return;
   }
   try {
     const { userId } = jwt.verify(tokenValue, process.env.JWT_SECRET);
-    const user = await userDB.findById(userId);
+    await User.findById(userId);
     res.locals.user = user;
     next();
   } catch (error) {
