cd109410b2c35be3f7e1fe8c604970df1e920c78
flypig-hub <103496007+flypig-hub@users.noreply.github.com>
2022-06-18 17:56:00 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -3,42 +3,48 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const UserRouter = require("./router/userRouter");
 const PostRouter = require("./router/postRouter");
-const likeRouter = require("./router/likeRouter");
-const connectDb = require("./database/database"); //몽고디비 스키마 연결
+const LikeRouter = require("./router/likeRouter");
+const connectDB = require("./database/database");
 const reqlogMiddleware = require("./middlewares/request-log-middleware");
 
 const port = 8080;
 
-connectDb(); //몽고디비 실행
-
 const corsOption = {
-    origin: "*",
+    origin: ["http://localhost:8080"],
     credentials: true,
 };
 //cors 설정
 
+
+
+connectDB();
+
+
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
+db.on("error", console.error.bind(console, "connection error:")); 
+
 
 const app = express();
 
 //body parser
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 //미들웨어 실행
 app.use(reqlogMiddleware);
 app.use(cors(corsOption));
 
 //라우터 등록
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.send("Backend Sever")
 });
+//app.use("/api", [PostRouter, LikeRouter, UserRouter]);
+//app.use의 사용방법 알아보세용
 app.use("/user", UserRouter);
 
 app.use("/post", PostRouter);
 
-app.use("/like", likeRouter);
+app.use("/like", LikeRouter);
 
 app.listen(port, () => {
     console.log(port, "포트로 서버가 켜졌어요~!")
