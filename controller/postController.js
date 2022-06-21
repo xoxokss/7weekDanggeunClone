2206eae0629696eba006d7f5ce8c09ae6edb3644
xoxokss <sangseon14@gmail.com>
2022-06-21 16:15:30 +0900

    REFACTOR : 전체 게시글 조회 API
    전체 게시글 API 기능 복구
    연습용 API 생성

--- controller/postController.js
+++ controller/postController.js
@@ -5,24 +5,37 @@ const User = require("../models/user");
 // 게시글 전체 조회 API
 
 async function allPost(req, res) {
-  let posts = await Post.find().sort({ createdAt: "asc" }).exec();
-
-  for (i = 0; i < posts.length; i++) {
-    let post = posts[i];
-
-    let postId = post.postId;
-    let likes = await Like.find({ postId: postId });
-
-    let likeNum = likes.length;
-    Object.assign(post, { likeNum: likeNum });
-    console.log(post, likeNum);
-  }
+  posts = await Post.find().sort({ createdAt: "asc" }).exec();
 
   res.status(200).send({
-    result: true,
-    post,
+    posts: posts.map((a) => ({
+      postId: a.postId,
+      title: a.title,
+      price: a.price,
+      postImg: a.postImg,
+      userLocation: a.userLocation,
+      likeNum: "0",
+      createdAt:
+        a.createdAt.toLocaleDateString("ko-KR") +
+        a.createdAt.toLocaleTimeString("ko-KR"),
+    })),
   });
 }
+
+// 전체게시글 조회 연습 지우지마세요.
+async function allPost2(req, res) {
+  const Posts = await Post.find().sort({ createdAt: "asc" }).exec();
+  // console.log(Posts) //  Posts = [{게시글1}, {게시글2}, {게시글3}]
+  const postsWithLike = [];
+  let likes = []
+  for (i = 0; i < Posts.length; i++) {
+    likes = await Like.find({ postId: Posts[i].postId })
+    postsWithLike.push();
+  }
+  console.log(likes)
+  res.status(200).send({ result:"hello"})
+  
+  }
 
 // 게시글 작성 API
 // figma에는 지역을 사용자에게 입력받게 되어있는데, 사용자 정보(/user/me)로 갖고가는게 맞지않나?
@@ -119,8 +132,8 @@ async function getPostDetail(req, res) {
   const { postId } = req.params;
   console.log(postId);
 
-   // 좋아요 수 : Like DB안에 postId가 갖고있는 데이터베이스 개수 세기
-  const likeN = await Like.find({postId}).exec();
+  // 좋아요 수 : Like DB안에 postId가 갖고있는 데이터베이스 개수 세기
+  const likeN = await Like.find({ postId }).exec();
   const likeNum = likeN.length; //
 
   // 내가 좋아요 했는지 확인. 좋아요 눌렀으면 1==true|| 안눌렀으면 0==false
@@ -151,7 +164,7 @@ async function getPostDetail(req, res) {
 }
 
 module.exports.allPost = allPost;
-
+module.exports.allPost2 = allPost2;
 module.exports.writePost = writePost;
 module.exports.getPostDetail = getPostDetail;
 module.exports.updatePost = updatePost;
