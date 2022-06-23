834f4571f2f7ccd02eed3a1c847ee72eb8068ce1
xoxokss <sangseon14@gmail.com>
2022-06-23 15:34:49 +0900

    FIX : Post model
    모델 스키마 복구

--- models/post.js
+++ models/post.js
@@ -2,40 +2,22 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema(
   {
-    postId: String,
-
-    userId: {
-      String,
-      required: true,
-    },
-    nickname: {
-      String,
-      required: true,
-    },
+    userId :String,
+    nickname : String,
     postImg: String,
-    title: {
-      String,
-      required: true,
-    },
+    title: String,
     category: String,
-    content: {
-      String,
-      required: true,
-    },
-    userLocation: {
-      String,
-      required: true,
-    },
+    content: String,
+    userLocation: String,
     // createdAt: { type: Date, default: Date.now }, //수동으로 날짜 생성하기, 날짜 UTC+8하는 방법 찾기
     price: String,
     tradeState: String, //(판매중=0, 거래중=1, 거래완료=2)
-    likeNum: Number,
+    likeNum : Number
   },
   {
     timestamps: true,
   }
 );
-
 
 PostSchema.virtual('postId').get(function () {
   return this._id.toHexString();
