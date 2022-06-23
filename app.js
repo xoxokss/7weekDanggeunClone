de9ca1ba2cc8651a86ac32cbc4303cb75d679386
xoxokss <sangseon14@gmail.com>
2022-06-23 10:24:35 +0900

    REFACTOR : app.js 리팩토링

--- app.js
+++ app.js
@@ -5,12 +5,50 @@ const UserRouter = require("./router/userRouter");
 const PostRouter = require("./router/postRouter");
 const LikeRouter = require("./router/likeRouter");
 const connectDB = require("./database/database");
+const reqlogMiddleware = require("./middlewares/request-log-middleware");
 const moment = require("moment");
 
-const reqlogMiddleware = require("./middlewares/request-log-middleware");
 require("dotenv").config(); // env 패키지 연결
 
 const app = express();
+
+//CORS 설정
+const corsOption = {
+  origin: [
+    "http://localhost:3000",
+    "http://spartastatic.s3-website.ap-northeast-2.amazonaws.com",
+    "http://localhost:8080",
+  ],
+  credentials: true,
+};
+
+connectDB();
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//미들웨어 실행
+app.use(reqlogMiddleware);
+app.use(cors(corsOption));
+
+// 최상위 URL 접속 메세지
+app.get("/", (req, res) => {
+  res.status(200).send("Backend Sever");
+});
+
+// 라우터 등록
+app.use("/api/user", UserRouter);
+
+app.use("/api/post", PostRouter);
+
+app.use("/api/like", LikeRouter);
+
+
+
 
 // moment 한국시간 설정
 require("moment-timezone");
@@ -20,7 +58,7 @@ moment.tz.setDefault("Asia/Seoul");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "*",
+    origin: "http://spartastatic.s3-website.ap-northeast-2.amazonaws.com",
     // methods: ["GET", "POST"]
   },
 });
@@ -28,7 +66,7 @@ const io = require("socket.io")(http, {
 /*
 // socket.io 별도 서버 연결 (4000번)
 const http2 = require("http").createServer();
-const io = require("socket.io")(http);
+const io = require("socket.io")(http2);
 
 http2.listen(4000, () => { console.log('소켓 서버 4000번')});
 */
@@ -77,42 +115,6 @@ io.on("connection", (socket) => {
     console.log("유저가 접속을 해제했습니다.");
   });
 });
-
-const corsOption = {
-  origin: [
-    "http://localhost:3000",
-    "http://spartastatic.s3-website.ap-northeast-2.amazonaws.com",
-    "http://localhost:8080",
-  ],
-  credentials: true,
-};
-//cors 설정
-
-connectDB();
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-
-//body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-//미들웨어 실행
-app.use(reqlogMiddleware);
-app.use(cors(corsOption));
-
-// 최상위 URL 접속 메세지
-app.get("/", (req, res) => {
-  // res.header("Access-Control-Allow-Origin", "http://spartastatic.s3-website.ap-northeast-2.amazonaws.com")
-  res.status(200).send("Backend Sever");
-});
-
-// 라우터 등록
-app.use("/api/user", UserRouter);
-
-app.use("/api/post", PostRouter);
-
-app.use("/api/like", LikeRouter);
 
 http.listen(process.env.PORT, () => {
   console.log(process.env.PORT, "번 포트를 통해 연결됐어요.");
